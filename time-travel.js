aa58a5b7c9f2b1de7236c4e24cb58e6a03f6ccb9
Arian Nazarian <anaz@MacBookPro.lan>
2025-02-19 18:33:02 -0800

    Fix: Ensure 'This time traveller’s watch...' toggles correctly and fades only once

--- time-travel.js
+++ time-travel.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+ddocument.addEventListener("DOMContentLoaded", () => {
     const frozenTime = new Date();
     const audio = document.querySelector("#eternal-audio");
     let isAudioPlaying = false;
@@ -194,7 +194,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 watchText.style.opacity = "1";
             }
         }
-    }    
+    }           
 
     function fadeInTravelQuote() {
         let travelQuote = document.querySelector("#travel-quote");
