e92d9adcf72d35890c6077fe2c555a8b1b4c2879
Arian Nazarian <anaz@MacBookPro.lan>
2025-03-29 15:57:17 -0700

    1.1s Header animation after 13.9s and every 6.4s thereafter for unclicked elements

--- main-site.js
+++ main-site.js
@@ -437,6 +437,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
+    if (!document.body.classList.contains('index-page')) return;
     const targets = {
         'link-personal': document.querySelector('#link-personal'),
         'link-work': document.querySelector('#link-work'),
